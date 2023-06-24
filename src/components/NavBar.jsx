8e12a35e71ca699a585f19905cb017bb1f5e5dc2
Huy Nguyen <113236181+cheesezzzz@users.noreply.github.com>
2023-06-24 21:56:14 +1000

    wip

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <>
-      <header className="sticky">
+      <header className="fixed">
         
         <nav>
             <Link to=".">Home</Link>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,21 +2,22 @@ export default function HomePage() {
   return (
     <>
       {/* Hero Section */}
-      <section className="p-3">
+      <section className="p-5">
         <div className="relative overflow-hidden rounded-md">
-          <div className="absolute bottom-0 text-9xl font-extrabold font-display uppercase p-4 text-white">
+          <div className="absolute bottom-0 text-9xl font-extrabold font-display uppercase p-4 text-white z-10">
             <h1>center</h1>
             <span></span>
             <h1>molasses</h1>
           </div>
           <video
+            className="brightness-75"
             width="1920"
             height="1080"
             autoPlay={true}
             muted
             loop={true}
           >
-            <source src="/src/assets/videos/hero.webm" type="video/webm" />
+            <source src="/src/assets/videos/hero1.webm" type="video/webm" />
           </video>
         </div>
       </section>
