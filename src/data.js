1ae19e983419b80fe73273e256a77cded0f695c7
Huy Nguyen <113236181+cheesezzzz@users.noreply.github.com>
2023-07-03 14:24:59 +1000

    images

---
+++ src/data.js
@@ -0,0 +1,53 @@
+const insightsData = [
+  {
+    id: 1,
+    image: "",
+    date: "",
+    type: "",
+    title: "Insight 1",
+    description: "Insight 1",
+    content: "This is the content of insight 1.",
+  },
+
+  {
+    id: 2,
+    image: "",
+    date: "",
+    type: "",
+    title: "Insight 1",
+    description: "Insight 1",
+    content: "This is the content of insight 1.",
+  },
+
+  {
+    id: 3,
+    image: "",
+    date: "",
+    type: "",
+    title: "Insight 1",
+    description: "Insight 1",
+    content: "This is the content of insight 1.",
+  },
+
+  {
+    id: 4,
+    image: "",
+    date: "",
+    type: "",
+    title: "Insight 1",
+    description: "Insight 1",
+    content: "This is the content of insight 1.",
+  },
+
+  {
+    id: 5,
+    image: "",
+    date: "",
+    type: "",
+    title: "Insight 1",
+    description: "Insight 1",
+    content: "This is the content of insight 1.",
+  },
+];
+
+export { insightsData };
